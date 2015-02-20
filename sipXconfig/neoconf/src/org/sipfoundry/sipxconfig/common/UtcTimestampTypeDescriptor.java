53d4480265db1fa0d69532f84bbc4fb66deb7cf1
dizzy <george@ezuce.com>
2015-02-20 13:01:33 +0200

    UC-3518: upgrade to 14.10 change working time attendant schedules
    - since Hibernate 3.6 types API has been changed, so custom TimestampType doesn't apply on 14.10 hibernate version. Rewritting UtcTimestampType with proper implementation

---
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/UtcTimestampTypeDescriptor.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2015 eZuce, Inc. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify it under
+ * the terms of the Affero General Public License (AGPL) as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ */
+package org.sipfoundry.sipxconfig.common;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.hibernate.type.descriptor.ValueBinder;
+import org.hibernate.type.descriptor.ValueExtractor;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
+import org.hibernate.type.descriptor.sql.BasicBinder;
+import org.hibernate.type.descriptor.sql.BasicExtractor;
+import org.hibernate.type.descriptor.sql.TimestampTypeDescriptor;
+
+public class UtcTimestampTypeDescriptor extends TimestampTypeDescriptor {
+
+    public static final UtcTimestampTypeDescriptor INSTANCE = new UtcTimestampTypeDescriptor();
+
+    private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+
+    public <X> ValueBinder<X> getBinder(final JavaTypeDescriptor<X> javaTypeDescriptor) {
+        return new BasicBinder<X>(javaTypeDescriptor, this) {
+            @Override
+            protected void doBind(PreparedStatement st, X value, int index, WrapperOptions options)
+                throws SQLException {
+                st.setTimestamp(index, javaTypeDescriptor.unwrap(value, Timestamp.class, options),
+                        Calendar.getInstance(UTC));
+            }
+        };
+    }
+
+    public <X> ValueExtractor<X> getExtractor(final JavaTypeDescriptor<X> javaTypeDescriptor) {
+        return new BasicExtractor<X>(javaTypeDescriptor, this) {
+            @Override
+            protected X doExtract(ResultSet rs, String name, WrapperOptions options) throws SQLException {
+                return javaTypeDescriptor.wrap(rs.getTimestamp(name, Calendar.getInstance(UTC)), options);
+            }
+        };
+    }
+}
