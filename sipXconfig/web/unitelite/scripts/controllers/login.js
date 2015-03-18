7e91d9ed9668362ad016b78892773fa17cffb924
Adrian Stanescu <stanescu.adrian@gmail.com>
2015-03-18 13:20:01 +0000

    XX-10965 - User Portal update
    UW-210 UI issues if a user has a long enough name;
    UW-177 Calling by name returns Number Not Found
    UW-222 Unite Lite: cannot call from dialpad

--- sipXconfig/web/unitelite/scripts/controllers/login.js
+++ sipXconfig/web/unitelite/scripts/controllers/login.js
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) eZuce, Inc. All rights reserved.
- * Contributed to SIPfoundry under a Contributor Agreement
- *
- * This software is free software; you can redistribute it and/or modify it under
- * the terms of the Affero General Public License (AGPL) as published by the
- * Free Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- */
-
 (function(){
 'use strict';
 
@@ -47,6 +32,8 @@ uw.controller('loginController', [
       $scope.error        = null;
       $scope.username     = $location.search()['uniteusername'];
       $scope.password     = $location.search()['unitepassword'];
+
+      // chatService.connect(angular.copy($scope.username), angular.copy($scope.password));
     }
 
 
@@ -153,6 +140,7 @@ uw.controller('loginController', [
           $scope.info   = null;
           $scope.error  = 'Invalid credentials';
         })
+      // chatService.connect(angular.copy($scope.username), angular.copy($scope.password));
     }
   }
 ]);
