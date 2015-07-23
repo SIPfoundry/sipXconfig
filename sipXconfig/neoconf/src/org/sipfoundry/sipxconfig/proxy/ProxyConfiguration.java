36cbbbe03183f05423bd52bdf532bfd43e7d6e48
SIPFoundry Dev <sipfoundrydev@gmail.com>
2015-07-23 10:21:53 +0800

    Removed natp plugin from proxy configuration

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/proxy/ProxyConfiguration.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/proxy/ProxyConfiguration.java
@@ -136,8 +136,9 @@ public class ProxyConfiguration implements ConfigProvider, ApplicationContextAwa
                 "$(sipx.SIPX_LIBDIR)/authplugins/libEnforceAuthRules.so");
         config.write("SIPX_PROXY_HOOK_LIBRARY.700_fromalias",
                 "$(sipx.SIPX_LIBDIR)/authplugins/libCallerAlias.so");
-        config.write("SIPX_PROXY_HOOK_LIBRARY.900_ntap",
-                "$(sipx.SIPX_LIBDIR)/authplugins/libNatTraversalAgent.so");
+        //
+        // config.write("SIPX_PROXY_HOOK_LIBRARY.900_ntap",
+        //        "$(sipx.SIPX_LIBDIR)/authplugins/libNatTraversalAgent.so");
         config.write("SIPX_PROXY_HOOK_LIBRARY.995_requestlinter",
                 "$(sipx.SIPX_LIBDIR)/authplugins/libRequestLinter.so");
         config.write("SIPX_PROXY_HOOK_LIBRARY.990_emerglineid",
