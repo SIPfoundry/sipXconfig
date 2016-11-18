ff4c69d8e5cc33d492a3292306c0ab647b5db11b
Ryan Colobong <ryan.colobong@sipfoundry.org>
2016-11-18 20:38:00 +0800

    Redmine #38: Configure RTP port range for SBC  - Change Address Id for RTP from natRtp to sbcPublicRtp

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/oss/OSSCoreManager.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/oss/OSSCoreManager.java
@@ -8,12 +8,12 @@ public interface OSSCoreManager {
     public static final AddressType PUBLIC_TCP_ADDRESS = AddressType.externalSipTcp("sbcPublicTCP");
     public static final AddressType PUBLIC_UDP_ADDRESS = AddressType.externalSipUdp("sbcPublicUDP");
     public static final AddressType PUBLIC_TLS_ADDRESS = AddressType.sipTls("sbcPublicTLS");
+    public static final AddressType PUBLIC_RTP_ADDRESS = new AddressType("sbcPublicRTP", "rtp:%s:%d", 30000,
+            AddressType.Protocol.udp);
     
     public static final AddressType INTERNAL_TCP_ADDRESS = AddressType.sipTcp("sbcInternalTCP");
     public static final AddressType INTERNAL_UDP_ADDRESS = AddressType.sipUdp("sbcInternalUDP");
-    
-    public static final AddressType PUBLIC_RTP_ADDRESS = new AddressType("natRtp", "rtp:%s:%d", 30000,
-            AddressType.Protocol.udp);
+
 
     public OSSCoreSettings getSettings();
 
