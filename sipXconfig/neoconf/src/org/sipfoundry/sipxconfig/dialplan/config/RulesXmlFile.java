f99833776ecae754aae1355f3d7e58886a09231a
Ryan Colobong <ryan.colobong@sipfoundry.org>
2018-03-19 23:13:29 +0800

    Add attribute to external mapping rules to specify if it will be appended top or bottom

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/dialplan/config/RulesXmlFile.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/dialplan/config/RulesXmlFile.java
@@ -13,10 +13,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentFactory;
@@ -52,6 +54,18 @@ public abstract class RulesXmlFile extends RulesFile {
      * @param mappings - root element of the document
      */
     protected void addExternalRules(Element mappings) {
+        addTopExternalRules(mappings);
+    }
+    
+    protected void addTopExternalRules(Element mappings) {
+        addExternalRules(mappings, AppendType.APPEND_TOP, true);
+    }
+    
+    protected void addBottomExternalRules(Element mappings) {
+        addExternalRules(mappings, AppendType.APPEND_BOTTOM, false);
+    }
+    
+    protected void addExternalRules(Element mappings, AppendType appendType, boolean appendByDefault) {
         if (m_externalRulesFileName == null) {
             return;
         }
@@ -61,12 +75,23 @@ public abstract class RulesXmlFile extends RulesFile {
             return;
         }
         SAXReader reader = new SAXReader();
+        reader.setMergeAdjacentText(true);
+        reader.setStripWhitespaceText(true);
         try {
             Document externalRules = reader.read(externalRulesFile);
             Element rootElement = externalRules.getRootElement();
             for (Iterator i = rootElement.elementIterator(); i.hasNext();) {
                 Element hostMatch = (Element) i.next();
-                mappings.add(hostMatch.detach());
+                Attribute attribute = hostMatch.attribute("append");
+                if(attribute != null) {
+                    if(StringUtils.equals(attribute.getValue(), appendType.getType())) {
+                        removeAllAttributes(hostMatch, "append");
+                        mappings.add(hostMatch.detach());        
+                    }
+                } else if(appendByDefault) {
+                    removeAllAttributes(hostMatch, "append");
+                    mappings.add(hostMatch.detach());
+                }
             }
         } catch (DocumentException e) {
             LOG.error("Cannot parse external rules file", e);
@@ -101,6 +126,29 @@ public abstract class RulesXmlFile extends RulesFile {
         String nameText = rule.getName();
         if (!StringUtils.isBlank(nameText)) {
             hostMatch.addComment(nameText);
+        }
+    }
+    
+    private void removeAllAttributes(Element element, String attributeName) {
+        for (Iterator i = element.attributeIterator(); i.hasNext();) {
+            Attribute attributeMatch = (Attribute) i.next();
+            if (StringUtils.equals(attributeMatch.getName(), attributeName)) {
+                element.remove(attributeMatch);
+            }
+        }
+    }
+    
+    enum AppendType {
+        APPEND_TOP("top"),
+        APPEND_BOTTOM("bottom");
+                
+        final String type;
+        private AppendType(String type) {
+            this.type = type;
+        }
+        
+        final String getType() {
+            return type;
         }
     }
 }
